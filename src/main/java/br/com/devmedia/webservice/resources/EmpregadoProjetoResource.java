5e6a632353ba89a669502cd5efe260d4e7e9aacb
Robson Rocha <robson.proxycode@gmail.com>
2018-03-17 02:47:40 -0300

    Adicionado métodos nos resources para criar, e exluir relacionamento entre projeto e empregado

--- src/main/java/br/com/devmedia/webservice/resources/EmpregadoProjetoResource.java
+++ src/main/java/br/com/devmedia/webservice/resources/EmpregadoProjetoResource.java
@@ -1,12 +1,20 @@
 package br.com.devmedia.webservice.resources;
 
+import java.util.List;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import br.com.devmedia.webservice.model.domain.Projeto;
 import br.com.devmedia.webservice.service.RlProjetoEmpregadoService;
 
+@Produces(MediaType.APPLICATION_JSON + ";charsert=utf-8")
 public class EmpregadoProjetoResource {
 
 	private RlProjetoEmpregadoService rlProjetoEmpregadoService = new RlProjetoEmpregadoService();
@@ -15,6 +23,18 @@ public class EmpregadoProjetoResource {
 	@Path("{projetoId}")
 	public Response save(@PathParam("projetoId") Long projetoId, @PathParam("empregadoId") Long empregadoId) {
 		rlProjetoEmpregadoService.saveRelationshipProjetoEmpregado(projetoId, empregadoId);
+		return Response.noContent().build();
+	}
+	
+	@GET
+	public List<Projeto> getProjetos(@PathParam("empregadoId") Long empregadoId){
+		return rlProjetoEmpregadoService.getProjetos(empregadoId);
+	}
+	
+	@DELETE
+	@Path("{projetoId}")
+	public Response delete(@PathParam("projetoId") Long projetoId, @PathParam("empregadoId") Long empregadoId) {
+		rlProjetoEmpregadoService.deleteRelationshiProjetoEmpregado(projetoId, empregadoId);
 		return Response.noContent().build();
 	}
 }
--- src/main/java/br/com/devmedia/webservice/resources/ProjetoEmpregadoResource.java
+++ src/main/java/br/com/devmedia/webservice/resources/ProjetoEmpregadoResource.java
@@ -1,13 +1,20 @@
 package br.com.devmedia.webservice.resources;
 
+import br.com.devmedia.webservice.model.domain.Empregado;
 import br.com.devmedia.webservice.service.RlProjetoEmpregadoService;
 
+import java.util.List;
+
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+@Produces(MediaType.APPLICATION_JSON + ";charsert=utf-8")
 public class ProjetoEmpregadoResource {
 
 	private RlProjetoEmpregadoService rlProjetoEmpregadoService = new RlProjetoEmpregadoService();
@@ -17,12 +24,17 @@ public class ProjetoEmpregadoResource {
 	public Response save(@PathParam("projetoId") Long projetoId, @PathParam("empregadoId") Long empregadoId) {
 		rlProjetoEmpregadoService.saveRelationshipProjetoEmpregado(projetoId, empregadoId);
 		return Response.noContent().build();
-	}
+	}	
 	
 	@GET
-	public void testeSubRecurso() {
-		System.out.println("Testando subrecurso");
+	public List<Empregado> getEmpregados(@PathParam("projetoId") Long projetoId){
+		return rlProjetoEmpregadoService.getEmpregados(projetoId);
 	}
 	
-	
+	@DELETE
+	@Path("{empregadoId}")
+	public Response delete(@PathParam("projetoId") Long projetoId, @PathParam("empregadoId") Long empregadoId) {
+		rlProjetoEmpregadoService.deleteRelationshiProjetoEmpregado(projetoId, empregadoId);
+		return Response.noContent().build();
+	}
 }
